b047a229a979d7d493a6f14bb77a85a9564f6948
Ambs04 <amberleejones2007@gmail.com>
2025-01-26 16:09:28 +0200

    return the bankBranchInstance and create a function that returns the branchInfo when called

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ class BankBranch {
       this.branchInfo = branchInfo;
       BankBranch.bankBranchInstance = this;
     }
+    //- Return the `bankBranchInstance` whether it's newly created or existing.
+    return BankBranch.bankBranchInstance;
   }
-  //    - Return the `bankBranchInstance` whether it's newly created or existing.
-
   // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
+  getBranchInfo() {
+    return this.branchInfo;
+  }
 }
 // 5. In the usage section:
 //    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
